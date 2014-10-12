4059222bbe6db9c67dec758069aba0bb8bbfc672
“Jen <mpjeno@gmail.com>
2014-10-11 22:27:28 -0500

    config for action mailer in each env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,6 +20,7 @@ FinalProject::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true 
+  config.action_mailer.default_url_options = { :host => "dev.gruplit.herokuapp.com" }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
